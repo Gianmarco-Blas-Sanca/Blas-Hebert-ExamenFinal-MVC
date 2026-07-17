f5c7b99a542ad6946c16795d07da9d3076550b00
Gianmarco Blas <gianmarcoblassanca@gmail.com>
2026-07-17 11:06:21 -0500

    Primer cambio en objeto

--- src/main/java/org/unisiga/model/MiembroUniversitario.java
+++ src/main/java/org/unisiga/model/MiembroUniversitario.java
@@ -1,9 +1,5 @@
 package org.unisiga.model;
 
-/**
- * Clase Abstracta Base que representa a cualquier miembro de la universidad.
- * [EVALUACIÓN]: El estudiante debe comprender el concepto de herencia y atributos protegidos.
- */
 public abstract class MiembroUniversitario {
     protected String rut;
     protected String nombre;
@@ -15,13 +11,8 @@ public abstract class MiembroUniversitario {
         this.correoInstitucional = correoInstitucional;
     }
 
-    /**
-     * TODO: Firma abstracta para el inicio de sesión.
-     * Cada subclase (Estudiante/Académico) debe sobreescribirla con sus reglas de negocio.
-     */
     public abstract boolean login(String password);
 
-    // Getters y Setters
     public String getRut() { return rut; }
     public void setRut(String rut) { this.rut = rut; }
     public String getNombre() { return nombre; }
